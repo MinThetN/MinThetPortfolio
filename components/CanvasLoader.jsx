6b42674e0fcd701abdc2fe633f927859ec0e4ac1
MinThetN <minthetn15@gmail.com>
2025-02-20 12:32:39 +0700

    updated

--- components/CanvasLoader.jsx
+++ components/CanvasLoader.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Html } from '@react-three/drei'
+import { Html, useProgress } from '@react-three/drei'
 
 const CanvasLoader = () => {
+    const { progress } = useProgress()
   return (
-    <div>
-        <Html> Canvas Loader </Html>
-    </div>
+        <Html
+            as='div'
+            center
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+            }}
+        > 
+            <span className='canvas-loader' />
+            <p style={{ fontSize: 14, color: '#F1F1F1', fontWeight: 800, marginTop: 40 }}>
+                {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
+            </p>
+        </Html>
   )
 }
 
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,26 +1,83 @@
 'use client'
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
 import HackerRoom from './HackerRoom'
+import CanvasLoader from './CanvasLoader'
+import { Leva, useControls } from 'leva'
 
 const Hero = () => {
+  const x = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+
+    rotationX: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    rotationY: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    rotationZ: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+
+    scale: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    }
+
+  })
   return (
     <section className='min-h-screen border-2 border-blue-600'>
-      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-mono'> Hi, I'm Min 
+      <div className='w-full mx-auto flex flex-col mt-5 c-space gap-3'>
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-mono'>Hi, I'm Min
           <span className='waving-hand'>👋</span>
+          
         </p>
         <p className='hero_tag text-gray_gradient'> Building & Brands </p>
       </div>
 
       <div className='w-full h-full absolute inset-0'>
+        <Leva />
         <Canvas className='w-full h-full'>
-          <Suspense fullback={null}>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <OrbitControls enableZoom={false} />
+            
+            {/* Fixed lighting setup */}
+            
+            <HackerRoom 
+              // scale={0.05} 
+              // position={[0, 0, 0]} 
+              // rotation={[0, 280, 0]}
+              position={[x.positionX, x.positionY, x.positionZ]} 
+              rotation={[x.rotationX, x. rotationY, x.rotationZ]} 
+              scale={[x.scale, x.scale, x.scale]}
+            />
+            <ambientLight intensity={1.0} color={'#ffffff'} />
+            <directionalLight position={[10, 10, 10]} intensity={1} />
 
           </Suspense>
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          <HackerRoom />
         </Canvas>
       </div>
     </section>
@@ -28,4 +85,3 @@ const Hero = () => {
 }
 
 export default Hero
-
